00ef1ea66877eb6fd6cc4f2cb86b22f1ad7d765c
kislayabhi <abhijeetkislay@gmail.com>
2015-11-02 16:36:15 -0600

    q4_main started

---
+++ ch_3/ques_4/q4_main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void f2s()
+{
+
+}
+
+void s23()
+{
+
+}
+
+int main()
+{
+	stack<int> first;
+	stack<int> second;
+	stack<int> third;
+
+	for(int i=0; i<10; ++i)
+	{
+		first.push(i);
+	}
+
+
+
+	// while(!first.empty())
+	// {
+	// 	cout<<first.top()<<endl;
+	// 	first.pop();
+	// }
+}

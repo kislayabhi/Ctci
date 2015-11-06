d38919db6f299f9919c4d8fae12881b0bb585b00
kislayabhi <abhijeetkislay@gmail.com>
2015-11-06 16:27:02 -0600

    freshly and correctly implemented

--- ch_4/bfs.cpp
+++ ch_4/bfs.cpp
@@ -1,40 +1,46 @@
 #include <iostream>
 #include <list>
+#include <algorithm>
+
+// This code is only compatible with C++ 11.
 
 // Global visited array
-bool visited[]={0, 0, 0, 0};
+bool visited[5]={0, 0, 0, 0, 0};
 
 // Dfs needs just the root of the graph.
 void Dfs(int root, std::list<int>* adj)
-{
-	// int i=0;
-	// while(i<4)
-	// {
-	// 	if(*((adj+i)->begin())==i)
-	// 		break;
-	// 	++i;
-	// }
-
-	// current list is adj+i
+{	
 	std::list<int>::iterator h_p, c_p;
 	h_p = (adj+root)->begin();
-	c_p = h_p;
-	visited[*h_p]=1;
-
-	//while
-	while(visited[*c_p]==1 && c_p!=(adj+root)->end())
+	while( std::any_of(std::begin(visited), std::end(visited), [](bool i){return i==0;}) )
 	{
-		++c_p;
+    	c_p = h_p;
+    	visited[*h_p]=1;
+    
+    	while(c_p!=(adj+root)->end() && visited[*c_p]==1)
+    	{
+    		++c_p;
+    	}
+    	
+    	if(c_p==(adj+root)->end())
+    		return;
+    	else
+    		{
+    			std::cout<<" -- "<<*c_p<<std::endl;
+    			Dfs(*c_p, adj);
+    		}
+    
+    
+        /*
+        std::cout<<"--------------------"<<std::endl;
+    	for(int i=0; i<5; i++)
+    	{
+    		std::cout<<visited[i] << " ";
+    	}
+    	std::cout<<" "<<std::endl;
+    	std::cout<<"--------------------"<<std::endl;
+    	*/
 	}
-
-	if(c_p==(adj+root)->end())
-		return;
-	else
-		{
-			std::cout<<" -- "<<*c_p<<std::endl;
-			Dfs(*c_p, adj);
-		}
-
 }
 
 int main()
@@ -47,35 +53,38 @@ int main()
 	//   3
 
 	// Make an array of Linked_list. Each contains the head node.
-	std::list<int>* adj= new std::list<int>[4];
+	std::list<int>* adj= new std::list<int>[5];
+	
+	// This doesn't work for graphs yet. only for trees.
 	adj->push_back(0);
 	adj->push_back(1);
-	adj->push_back(2);
+	//adj->push_back(2);
 
 	(adj+1)->push_back(1);
-	(adj+1)->push_back(0);
+	(adj+1)->push_back(4);
+	//(adj+1)->push_back(0);
 	(adj+1)->push_back(3);
+	//(adj+1)->push_back(2);
 
 	(adj+2)->push_back(2);
-	(adj+2)->push_back(0);
+	//(adj+2)->push_back(0);
+	(adj+2)->push_back(4);
+	//(adj+2)->push_back(3);
 
 	(adj+3)->push_back(3);
 	(adj+3)->push_back(1);
+	(adj+3)->push_back(4);
 
-/*	
-for(int i=0; i<4; ++i)
-{
-	for(std::list<int>::iterator it=(adj+i)->begin(); it!=(adj+i)->end(); ++it)
-	{
-		std::cout<<*it<<std::endl;
-	}
-	std::cout<<"\n"<<std::endl;
-}
-*/
+    (adj+4)->push_back(4);
+    (adj+4)->push_back(2);
+    (adj+4)->push_back(3);
+    (adj+4)->push_back(1);
+
 	Dfs(0, adj);
 
 	for(int i=0; i<4; i++)
 	{
 		std::cout<<visited[i]<<std::endl;
 	}
+
 }
